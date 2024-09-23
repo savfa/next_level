9a60a70c484571c83ff760d2e31c4101827d514d
savfa <savfa@yandex.ru>
2024-09-23 20:22:13 +0300

    add skeleton components from fetch data dashboard home page

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -3,7 +3,7 @@
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
+    name: 'Иванов Иван',
     email: 'user@nextmail.com',
     password: '123456',
   },
@@ -12,37 +12,37 @@ const users = [
 const customers = [
   {
     id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
-    name: 'Evil Rabbit',
+    name: 'Сидоров Иван',
     email: 'evil@rabbit.com',
     image_url: '/customers/evil-rabbit.png',
   },
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
+    name: 'Кузнецова Анна',
     email: 'delba@oliveira.com',
     image_url: '/customers/delba-de-oliveira.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
+    name: 'Смирнов Дмитрий',
     email: 'lee@robinson.com',
     image_url: '/customers/lee-robinson.png',
   },
   {
     id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
-    name: 'Michael Novotny',
+    name: 'Васильев Михаил',
     email: 'michael@novotny.com',
     image_url: '/customers/michael-novotny.png',
   },
   {
     id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
-    name: 'Amy Burns',
+    name: 'Попова Екатерина',
     email: 'amy@burns.com',
     image_url: '/customers/amy-burns.png',
   },
   {
     id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
-    name: 'Balazs Orban',
+    name: 'Морозов Сергей',
     email: 'balazs@orban.com',
     image_url: '/customers/balazs-orban.png',
   },
@@ -130,18 +130,18 @@ const invoices = [
 ];
 
 const revenue = [
-  { month: 'Jan', revenue: 2000 },
-  { month: 'Feb', revenue: 1800 },
-  { month: 'Mar', revenue: 2200 },
-  { month: 'Apr', revenue: 2500 },
-  { month: 'May', revenue: 2300 },
-  { month: 'Jun', revenue: 3200 },
-  { month: 'Jul', revenue: 3500 },
-  { month: 'Aug', revenue: 3700 },
-  { month: 'Sep', revenue: 2500 },
-  { month: 'Oct', revenue: 2800 },
-  { month: 'Nov', revenue: 3000 },
-  { month: 'Dec', revenue: 4800 },
+  { month: 'Янв', revenue: 2000 },
+  { month: 'Фев', revenue: 1800 },
+  { month: 'Мар', revenue: 2200 },
+  { month: 'Апр', revenue: 2500 },
+  { month: 'Май', revenue: 2300 },
+  { month: 'Июн', revenue: 3200 },
+  { month: 'Июл', revenue: 3500 },
+  { month: 'Авг', revenue: 3700 },
+  { month: 'Сен', revenue: 2500 },
+  { month: 'Окт', revenue: 2800 },
+  { month: 'Ноя', revenue: 3000 },
+  { month: 'Дек', revenue: 4800 },
 ];
 
 export { users, customers, invoices, revenue };
