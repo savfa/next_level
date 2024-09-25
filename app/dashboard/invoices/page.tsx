4417046cf78d78a34d0f6f211eef98427e092f97
savfa <savfa@yandex.ru>
2024-09-26 00:17:49 +0300

    add actions crud from dashboard invoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -29,8 +29,8 @@ export default async function Page({ searchParams }: {
         <CreateInvoice />
       </div>
         <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-        <Table query={query} currentPage={currentPage} />
-      </Suspense>
+          <Table query={query} currentPage={currentPage} />
+        </Suspense>
       <div className="mt-5 flex w-full justify-center">
          <Pagination totalPages={totalPages} />
       </div>
